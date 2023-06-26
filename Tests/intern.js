9c3f4f8cd15a1a9b2b1a78468d5c1e1762378ea8
Dior G <diorgoolsby@Hotmail.com>
2023-06-25 21:46:06 -0700

    More intern.js work

--- Tests/intern.js
+++ Tests/intern.js
@@ -3,14 +3,19 @@ const Intern = require("../lib/intern");
 describe("Intern", () => {
     describe("Initialization", () => {
         it("should inherit values and methods from the parent class 'Employee'", () => {
-            const intern = new Intern("Tiffany", 7, "tifftaff@gmail.com", "UofA");
+            const intern = new Intern("Tiffany", 6, "tifftaff@gmail.com", "UofA");
 
             expect(intern.getName()).toEqual("Tiffany");
             expect(intern.getId()).toEqual(7);
             expect(intern.getEmail()).toEqual("tifftaff@gmail.com");
         });
         it("should add the additional property 'school' to the given object", () => {
-            const intern = new Intern("Tiffany", 7, "tifftaff@gmail.com", "UofA");
+            const intern = new Intern("Tiffany", 6, "tifftaff@gmail.com", "UofA");
 
             expect(intern.school).toEqual("UofA");
+        });
+        it("should throw an error if 'school' is not a string", () => {
+            const err = new Error("Expected parameter 'school' to be a string");
+              
+            expect(() => new Intern("Tiffany", 6, "tifftaff@gmail.com", 25)).toThrowError(err);
         });
