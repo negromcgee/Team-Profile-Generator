e8c52120d5f82226a90e47b27a6da5593f7d44c0
Dior G <diorgoolsby@Hotmail.com>
2023-06-25 21:48:48 -0700

    finishing intern.js

--- Tests/intern.js
+++ Tests/intern.js
@@ -19,3 +19,17 @@ describe("Intern", () => {
               
             expect(() => new Intern("Tiffany", 6, "tifftaff@gmail.com", 25)).toThrowError(err);
         });
+        it("should override 'role' to have the value 'Intern'", () => {
+            const intern = new Intern("Tiffany", 6, "tifftaff@gmail.com", "UofA");
+
+            expect(intern.getRole()).toEqual("Intern");
+        });
+    });
+    describe("getSchool", () => {
+        it("should return the 'school' value of a given object", () => {
+            const intern = new Intern("Tiffany", 6, "tifftaff@gmail.com", "UofA");
+
+            expect(intern.getSchool()).toEqual("UofA");
+        });
+    });
+});
