55cec81550ffb17d31a90c789a4c76fd7ff60b8f
Dior G <diorgoolsby@Hotmail.com>
2023-06-25 21:33:43 -0700

    Adding to manager.js

--- Tests/manager.js
+++ Tests/manager.js
@@ -4,3 +4,11 @@ describe("Manager", () => {
     describe("Initialization", () => {
         it("should inherit values and methods from the parent class 'Employee'", () => {
             const manager = new Manager("Ruby", 5, "ruby@Yahoo.com", 55);
+
+            const Manager = require("../lib/manager");
+
+            expect(manager.getName()).toEqual("Ruby");
+            expect(manager.getId()).toEqual(5);
+            expect(manager.getEmail()).toEqual("Ruby@Yahoo.com");
+        });
+        
