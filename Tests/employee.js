7c72210e23b8cfc67d88809bd1dc60c98be5b60b
Dior G <diorgoolsby@Hotmail.com>
2023-06-26 19:06:39 -0700

    adding name & ID functionality

--- Tests/employee.js
+++ Tests/employee.js
@@ -10,3 +10,19 @@ describe("Employee", () => {
             expect(employee.email).toEqual(expect.any(String));
         });
     });
+
+    describe("Name", () => {
+        test("gets employee name", () => {
+            const employee = new Employee("Tiffany", 6, "tifftaff@gmail.com");
+
+            expect(employee.getName()).toEqual(expect.any(String));
+        });
+    });
+
+    describe("ID", () => {
+        test("gets employee ID", () => {
+            const employee = new Employee("Tiffany", 6, "tifftaff@gmail.com");
+
+            expect(employee.getId()).toEqual(expect.any(Number));
+        });
+    });
